7101fd8c1455ae3743c82f2bf431573ed1d60006
Xavier Coulon <xcoulon@redhat.com>
2020-03-04 16:00:27 +0100

    add badges on README and remove unused files (#9)
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/lint.mk
+++ make/lint.mk
@@ -1,15 +1,5 @@
 .PHONY: lint
 ## Runs linters on Go code files and YAML files
-lint: lint-go-code lint-yaml
-
-YAML_FILES := $(shell find . -type f -regex ".*y[a]ml" -print)
-.PHONY: lint-yaml
-## runs yamllint on all yaml files
-lint-yaml: ${YAML_FILES}
-	yamllint -c .yamllint $(YAML_FILES)
-
-.PHONY: lint-go-code
-## Checks the code with golangci-lint
-lint-go-code:
+lint:
 	go get github.com/golangci/golangci-lint/cmd/golangci-lint
 	${GOPATH}/bin/golangci-lint ${V_FLAG} run
