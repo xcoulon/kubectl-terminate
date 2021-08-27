e6263b4a1cffe5716f44e0a4a546da5b6ecaa399
Xavier Coulon <xcoulon@redhat.com>
2021-08-27 15:47:01 +0200

    prepare for first release
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/go.mk
+++ make/go.mk
@@ -6,18 +6,34 @@ GO_PACKAGE_PATH ?= github.com/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}
 GO111MODULE?=on
 export GO111MODULE
 
+CUR_DIR=$(shell pwd)
+INSTALL_PREFIX=$(CUR_DIR)/bin
+
+ifeq ($(OS),Windows_NT)
+BINARY_PATH=$(INSTALL_PREFIX)/kubectl-terminate.exe
+else
+BINARY_PATH=$(INSTALL_PREFIX)/kubectl-terminate
+endif
+
 .PHONY: build 
 ## Build the operator
 build: 
-	@echo "building..."
-	@-CGO_ENABLED=0 \
-		go build \
-		-o $(OUT_DIR)/kubectl-terminate \
+	$(eval BUILD_COMMIT:=$(shell git rev-parse --short HEAD))
+	$(eval BUILD_TAG:=$(shell git tag --contains $(BUILD_COMMIT)))
+	$(eval BUILD_TIME:=$(shell date -u '+%Y-%m-%dT%H:%M:%SZ'))
+	@echo "building with commit:$(BUILD_COMMIT) / tag:$(BUILD_TAG) / time:$(BUILD_TIME)"
+	@CGO_ENABLED=0 \
+		go build -ldflags \
+		"-X github.com/xcoulon/kubectl-terminate/BuildCommit=$(BUILD_COMMIT) \
+	    -X github.com/xcoulon/kubectl-terminate/main.BuildTag=$(BUILD_TAG) \
+	    -X github.com/xcoulon/kubectl-terminate/main.BuildTime=$(BUILD_TIME)" \
+		-o $(BINARY_PATH) \
 		cmd/main.go
+	@echo "$(BINARY_PATH) is ready to use"
 
 .PHONY: install
 ## Builds and installs the operator in $GOPATH/bin
 install: build
 	@echo "installing..."
-	 mv $(OUT_DIR)/kubectl-terminate $(GOPATH)/bin
+	 mv $(BINARY_PATH) $(GOPATH)/bin
 
